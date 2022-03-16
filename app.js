89810ef3e20666c30c20a23412275c8799f8061c
Gabs008 <nascimentogabrielle26@gmail.com>
2022-03-16 18:10:56 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const {principal}=require('./src/index.js')
+
+principal()
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const moment = require('moment')
+const { exibeTela, calculaValor } = require('../lib/funcao.js')
+const { produtos } = require('../model/dados')
+exports.principal=()=>{
+exibeTela('----------- Mercado DH ----------')
+
+const data = moment().format('DD/MM/YYYY hh:mm')
+exibeTela('----- Lista Produtos - ' + data + ' -----')
+
+
+let valorTotal = 0;
+for (let i=0; i < produtos.length; i++) {
+
+  valorTotal += calculaValor(produtos[i].qtd, produtos[i].valor)
+
+  exibeTela(`${produtos[i].id} - ${produtos[i].nome} - ${produtos[i].qtd}`)
+}
+exibeTela('---> Total R$: ' + valorTotal)
+}
